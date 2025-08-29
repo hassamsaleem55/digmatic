1232a22f49d97849d94b02b22a5a699e32504422
hassamsaleem55 <73222564+hassamsaleem55@users.noreply.github.com>
2025-08-30 01:25:38 +0500

    one experiment: dlt vercel file from frontend and updated the backend vercel file as per vercel documentation. Also made changes in index.js file

--- api/index.js
+++ api/index.js
@@ -1,45 +1,18 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-// // Routes
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hello from backend!" });
-// });
-
-// app.get("/api/users", (req, res) => {
-//   res.json([
-//     { id: 1, name: "Hassam" },
-//     { id: 2, name: "Saleem" }
-//   ]);
-// });
-
-// // ✅ Localhost only
-// if (process.env.NODE_ENV !== "production") {
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => {
-//     console.log(`Backend running locally on http://localhost:${PORT}/api`);
-//   });
-// }
-
-// // ✅ Vercel expects this export
-// module.exports = app;
-
-
-const express = require('express');
-const app = express()
-const PORT = 3000
-
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
 
-app.get("/api/users", (req, res) => {
+app.get("/users", (req, res) => {
   res.json([
     { id: 1, name: "Hassam" },
-    { id: 2, name: "Saleem" }
+    { id: 2, name: "Saleem" },
   ]);
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
