51fa7f3072ceec9a9642f2b120dacfb7aea340f3
hassamsaleem55 <73222564+hassamsaleem55@users.noreply.github.com>
2025-08-31 00:45:25 +0500

    for testing purpose

--- api/index.js
+++ api/index.js
@@ -1,35 +1,44 @@
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import connectDB from "./db.js";
+// import bannerRoutes from "./routes/banner.js";
+// import serverless from "serverless-http";
+
+// dotenv.config();
+
+// const app = express();
+// app.use(express.json());
+
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "*",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+// await connectDB();
+
+// app.use(bannerRoutes);
+// app.get("/", (_req, res) =>
+//   res.json({ success: true, message: "Welcome to backend" })
+// );
+
+// export default serverless(app);
+
+// if (process.env.NODE_ENV === "development") {
+//   const PORT = process.env.PORT;
+//   app.listen(PORT, () => {
+//     console.log(`Local server running on http://localhost:${PORT}`);
+//   });
+// }
+
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./db.js";
-import bannerRoutes from "./routes/banner.js";
-import serverless from "serverless-http";
-
-dotenv.config();
-
 const app = express();
-app.use(express.json());
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-await connectDB();
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.use(bannerRoutes);
-app.get("/", (_req, res) =>
-  res.json({ success: true, message: "Welcome to backend" })
-);
-
-export default serverless(app);
-
-if (process.env.NODE_ENV === "development") {
-  const PORT = process.env.PORT;
-  app.listen(PORT, () => {
-    console.log(`Local server running on http://localhost:${PORT}`);
-  });
-}
+export default app;
